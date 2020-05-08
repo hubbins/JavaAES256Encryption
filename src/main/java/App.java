56612dbcfba637370e834bf6b0906c0232fffb30
Sean OConnor <soconnor@yahoo.com>
2020-05-08 09:30:11 -0500

    Added IV to data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,11 @@ import org.jasypt.util.text.AES256TextEncryptor;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
@@ -21,6 +25,28 @@ public class App {
         System.out.println("Encrypted: " + output);
         output = decrypt2(output, key16, iv);
         System.out.println("Decrypted: " + output);
+
+        output = encrypt3(input, key16);
+        System.out.println("Encrypted: " + output);
+    }
+
+    // returns 16 byte init vector followed by encrypted data
+    public static String encrypt3(final String input, final String key) {
+        try {
+            if (key.length() != 16)
+                throw new RuntimeException("Key must be 16 characters");
+            String iv = getInitVector();
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes(StandardCharsets.UTF_8));
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            byte[] encrypted = cipher.doFinal(input.getBytes());
+            return Base64.getEncoder().encodeToString(concat(iv.getBytes(StandardCharsets.UTF_8), encrypted));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static String encrypt2(final String input, final String key, final String iv) {
@@ -65,5 +91,27 @@ public class App {
         textEncryptor.setPassword(key);
         var decryptedText = textEncryptor.decrypt(input);
         return decryptedText;
+    }
+
+    // concatenate byte arrays into a single result
+    private static byte[] concat(byte[] ...arrays) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        for (byte[] a:arrays) {
+            outputStream.write(a);
+        }
+
+        return outputStream.toByteArray();
+    }
+
+    private static String getInitVector() {
+        // return 16 random letters
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < 16; i++) {
+            int r = random.nextInt(26);
+            buffer.append((char)(r + 97));  // 'a'
+        }
+
+        return buffer.toString();
     }
 }
